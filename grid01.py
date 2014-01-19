bb924e55fa72f74bfc3990db2024e3ff2af9b65f
hknunes <hknunes@gmail.com>
2014-01-19 18:49:19 +0000

    001
    Tabuleiro, introduçao de imgs random, troca de imgs, colunaRemove

--- grid01.py
+++ grid01.py
@@ -1,107 +1,115 @@
 ﻿import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite, RenderUpdates
+import random
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-numLinhas = 5   # A tabela tem 8 linhas
+numLinhas = 5   # A tabela tem 5 linhas
 numColunas = 5  # A tabela tem 5 colunas
-Tab = [[[74 * i, 74 * j] for j in range(numColunas)] for i in range(numLinhas)]
+objectivo = 3   # Objectivo do jogo
 
-im0 = pygame.image.load('im0.gif')
+def tensDeRemover(Tab, nlinhas, ncolunas, objectivo):
 
-# inicia a linha e coluna dum circulo que inicialmente está seleccionado
-xSele = 0
-ySele = 0
+    RemoveColuna = set()
+
+    for i in range(nlinhas):
+        for j in range(ncolunas):
+            cont = 1
+            k = i + 1
+            while k < nlinhas and Tab[k][j][2] == Tab[i][j][2]:
+                cont += 1
+                k += 1
+            k = i - 1
+            while k >= 0 and Tab[k][j][2] == Tab[i][j][2]:
+                cont += 1
+                k -= 1
+            if cont >= objectivo:
+                RemoveColuna.add(j)
+    return RemoveColuna
+
+cor = [pygame.image.load("im0.gif"), pygame.image.load("im1.gif"), pygame.image.load("im2.gif"),
+       pygame.image.load("im3.gif")]    # É desta lista que se escolhe as cores para os circulos
+
+# # Tab = [[[74 * i, 74 * j] for j in range(numColunas)] for i in range(numLinhas)]
+
+# incia motor de jogo
+pygame.init()
+
+# define ecra
+screen = pygame.display.set_mode([500,500])
+
+# escrever titulo da janela
+pygame.display.set_caption("Grid or Not")
+
+Tab=[]
+for j in range(numColunas):
+    linha = []
+    for i in range(numLinhas):
+        linha.append([74*i, 74*j, random.randrange(len(cor))])
+    Tab.append(linha)
 
 # inicia a linha e coluna do circulo onde está o cursor do rato
 xPre = 0
 yPre = 0
 
-# existe um circulo seleccionado  
-Selected = True
-
-# Inicia Pygame
-pygame.init()
-
-# Define o ecrã
-size = [370, 370]
-screen = pygame.display.set_mode(size)
-
-# escreve titulo da janela 
-pygame.display.set_caption("Grid - 0.0")
-
-#coordenadas do rato
-pos = pygame.mouse.get_pos()
-
-# Create a red player block
-seta = pygame.draw.rect(screen, red, ((pos[0], pos[1]), (5, 5)))
-
-
-#Para entrar em loop, até que a janela seja fechada.
+# Inicia Loop do jogo
 done = False
 
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-
-# -------- Main Program Loop -----------
 while not done:
-    for event in pygame.event.get():    # User did something
-        if event.type == pygame.QUIT:   # If user clicked close
-            done = True     # Flag that we are done so we exit this loop
 
-    # Clear the screen
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Limpa ecra
     screen.fill(black)
-    #coordenadas do rato
-    pos = pygame.mouse.get_pos()
 
-    #posição do rato
+    # Função que determina linhas ou colunas a remover
+    ColunasRem = tensDeRemover(Tab, numLinhas, numColunas, objectivo)
+
+    print("Colunas a alterar:", ColunasRem)
+
+    #
+    # Aqui deves remover as linhas e colunas e actualizar tabela de circulos
+    #
+
+    pos = pygame.mouse.get_pos()    # coordenadas do rato
+
+    # posiçao do rato
     xR = pos[0]
     yR = pos[1]
 
-    # Set the player object to the mouse location
-    seta.x = pos[0]
-    seta.y = pos[1]
-
-    # --- Desenha grelha --- #
+    # Desenha o estado actual do tabuleiro
     for i in range(numLinhas):
         for j in range(numColunas):
-            x = Tab[i][j][0]
-            y = Tab[i][j][1]
-
-            square = screen.blit(im0, (x,y))
-
+            x = Tab[i][j][0]      # vértice sup. esquerdo x
+            y = Tab[i][j][1]      # vértice sup. esquerdo y
+            c = cor[Tab[i][j][2]]   # cor
+            screen.blit(c, (x, y))   # desenho as minhas peças
+            # Determina linha e coluna da casa onde o cursor está posicionado
             if xR > x and xR < x + 74 and yR > y and yR < y + 74:
                 xPre = j
                 yPre = i
-                ##/-------------/##
 
     mousestat = pygame.mouse.get_pressed()  # teclas do rato
-    # Selecciona o circulo onde o cursor está posicionado
+
+    # Caso seja seleccionado um circulo, desenha quadrado
+    # vermelho que o envolva e troca a sua cor
     if mousestat[0]:
-        xSele = xPre
-        ySele = yPre
+        ValX = Tab[yPre][xPre][0]   # Coordenada x
+        ValY = Tab[yPre][xPre][1]   # Coordenada y
+        Ncor = Tab[yPre][xPre][2] + 1   # Roda a cor do disco
+        if Ncor > len(cor) - 1:
+            Ncor = 0
+        Tab[yPre][xPre][2] = Ncor     # Altera cor
+        pygame.draw.rect(screen, red, [ValX, ValY, 100, 100], 4)
 
-        ValX = Tab[ySele][xSele][0]
-        ValY = Tab[ySele][xSele][1]
-
-
-        # Caso esteja seleccionado um circulo, desenha quadrado
-    # vermelho que o envolva          
-    if Selected:
-        ValX = Tab[ySele][xSele][0]
-        ValY = Tab[ySele][xSele][1]
-
-        pygame.draw.rect(screen, black, [ValX, ValY, 74, 74], 2)
-        #squareR = pygame.draw.rect(screen, red, ((ValX+20,ValY+20),(60,60)))
-
-    # Limit to 20 frames per second
-    clock.tick(15)
-
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.update()
+    pygame.display.flip()
+    clock.tick(10)
 
 pygame.quit()
